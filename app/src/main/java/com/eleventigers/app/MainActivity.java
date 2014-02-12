2f237f781c42fca0862446669c5866757a4d57e0
eleventigers <phonodelik@gmail.com>
2014-02-12 14:22:13 +0000

    added menu options and toast to display their titles

--- app/src/main/java/com/eleventigers/app/MainActivity.java
+++ app/src/main/java/com/eleventigers/app/MainActivity.java
@@ -1,15 +1,18 @@
 package com.eleventigers.app;
 
+import android.app.Application;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -25,25 +28,32 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_search:
+            case R.id.action_settings:
+                notifyMenuOptionSelection(item);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void notifyMenuOptionSelection(MenuItem item) {
+        Toast.makeText(getApplicationContext(), item.getTitle(), Toast.LENGTH_SHORT)
+                .show();
     }
 
     /**
